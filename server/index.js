69be622b01c14ab35480725a71c44b64e8a90648
Walter Zhang <walter.zhangfang@gmail.com>
2022-06-01 20:24:23 +1200

    Building the connection with MongoDB.

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://memory-admin_01:n8z5UBRHHPoCQIiR@cluster0.fj0642b.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+//The "useFindAndModify" is deprecated now.
+// mongoose.set('useFindAndModify', false);
